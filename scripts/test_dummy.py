4a787f6a65791cec1507220f953cce831215bd33
elkoled <elkoled@gmail.com>
2023-09-21 12:53:56 +0200

    Update test_dummy.py

--- scripts/test_dummy.py
+++ scripts/test_dummy.py
@@ -3,7 +3,7 @@ import unittest
 class TestDummy(unittest.TestCase):
     def test_dummy(self):
         """Test dummy."""
-        self.assertEqual(true, true)
+        self.assertEqual(1, 1)
 
 if __name__ == '__main__':
     unittest.main()
